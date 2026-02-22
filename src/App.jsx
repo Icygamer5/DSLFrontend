46a0bea06cece1c1b752955941f95480873970a4
Angel Rodriguez <angel@Angels-MacBook-Pro.local>
2026-02-22 02:58:25 -0500

    Setinal AI Chatbot integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import LandingPage from './components/LandingPage'
+import SentinelChat from "./components/SentinelChat";
 
 function App() {
-  return <LandingPage />
+  return (
+    <>
+      <LandingPage />
+      <SentinelChat />
+    </>
+  );
 }
 
 export default App
